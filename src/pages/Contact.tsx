bf5373587d17bc5651c0367c9989f5d491dbc11b
Rajeev Ranjan <ranjan@Rajeevs-MacBook-Air-3.local>
2025-10-17 16:30:03 +0530

    new commit2

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -24,7 +24,7 @@ const Contact = () => {
     setStatus("Sending...");
 
     try {
-      const res = await fetch("http://localhost:3000/send", { // replace with your backend URL
+      const res = await fetch("https://manasvi-g9nu.vercel.app/send", { // replace with your backend URL
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
